cdf92679992b426594eddae38f1a0efea93e0d07
mahdi6788 <mjalalimech@gmail.com>
2025-06-05 16:29:28 +0330

    fix: add favicon link in the head of the RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
       <Toaster />
       <ClerkProvider>
         <html lang="en" suppressHydrationWarning>
+          <head>
+            <link rel="icon" href="/favicon.ico" />
+          </head>
           <body
             className={font.className}
           >
